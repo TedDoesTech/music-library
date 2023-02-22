650c6c497e74d71e80e84ec1cc25694dab663ffa
Ed Crowther <edward930@hotmail.co.uk>
2023-02-22 19:52:10 +0000

    test passing and create artist scripts working fine

--- src/app.js
+++ src/app.js
@@ -1,15 +1,10 @@
 const express = require('express');
+const artistRouter = require('./routes/artist');
 
 const app = express();
 
 app.use(express.json());
 
-//------------------
+app.use('/artists', artistRouter);
 
-app.get('/', (req, res) => {
-    res.status(200).json({ result: "Hello World" })
-})
-
-//------------------
-
-module.exports = app
+module.exports = app;
---
+++ src/controllers/artist.js
@@ -0,0 +1,14 @@
+const db = require('../db/index.js');
+
+const createArtist = async (req, res) => {
+    const { name, genre } = req.body
+  
+    try {
+      const { rows: [ artist ] } = await db.query(`INSERT INTO Artists (name, genre) VALUES ('${name}', '${genre}') RETURNING *`)
+      res.status(201).json(artist)
+    } catch (err) {
+      res.status(500).json(err.message)
+    }
+  }
+  
+  module.exports = { createArtist }
