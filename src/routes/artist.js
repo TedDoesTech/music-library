30c702b6a451b79c00c3b13d34c6fdfd684d8ba3
Ed Crowther <edward930@hotmail.co.uk>
2023-02-25 19:28:56 +0000

    reading artists working

--- src/routes/artist.js
+++ src/routes/artist.js
@@ -4,5 +4,6 @@ const artistController = require('../controllers/artist');
 const router = express.Router();
 
 router.post('/', artistController.createArtist);
+router.get('/', artistController.getAllArtists);
 
 module.exports = router;
